dfb824aa7a3c7f3f84d51a8e5fbd84f91107530b
Rajat Sharma <rajat.sharma.public@gmail.com>
2026-04-26 00:13:22 +0530

    Add: pascals-triangle-ii - submission-0

---
+++ Data Structures & Algorithms/pascals-triangle-ii/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    unordered_map<int, vector<int>> store;
+    int curr = 0;
+
+    vector<int> getRow(int rowIndex) {
+        int numRows = rowIndex+1;
+        vector<vector<int>> vec;
+        if(store.find(numRows) == store.end()) {
+            if(curr == 0) {
+                store[1] = vector<int>{1};
+                curr = 1;
+            }
+            for(int len=curr+1; len<=numRows; len++) {
+                vector<int> arr;
+                arr.push_back(1);
+                for(int i=1; i<store[len-1].size(); i++) {
+                    arr.push_back(store[len-1][i-1]+store[len-1][i]);
+                }
+                arr.push_back(1);
+                store[len] = arr;
+            }
+        }
+
+        curr = numRows;
+        return store[numRows];
+    }
+};
